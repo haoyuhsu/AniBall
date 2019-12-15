4b2caa31d26fdc6c72073ece78e9de65d60e007a
MaxHsu88 <maxsyu88@gmail.com>
2019-12-15 22:16:43 +0800

    Add Deformation Interval to Avoid Latnecy

--- Assets/Scripts/Mesh Deformation/TrigMeshDeformer.cs
+++ Assets/Scripts/Mesh Deformation/TrigMeshDeformer.cs
@@ -6,18 +6,32 @@ public class TrigMeshDeformer : MonoBehaviour
 {
     MeshDeformer[] meshDeformers;
 
+    float cooldownTime = 0f;            // 避免頻繁碰撞使得Mesh更新次數太大, 讓遊戲卡頓
+    float deformInterval = 0.1f;        // 每次Update Mesh之間的時間間隔
+
     void Start()
     {
         meshDeformers = GetComponentsInChildren<MeshDeformer>();
     }
 
+    void FixedUpdate()
+    {
+        if (cooldownTime <= 0f)
+            return;
+        cooldownTime -= Time.deltaTime;
+    }
+
 	void OnCollisionEnter(Collision other)
 	{
-        /* 當碰撞發生時, 把碰撞點資訊給到各MeshDeformer去做Vertex更新 */
-		foreach (MeshDeformer meshDeformer in meshDeformers)
+        if (cooldownTime <= 0f)
         {
-            meshDeformer.isCollided = true;
-            meshDeformer.Deforming(other);
+            /* 當碰撞發生時, 把碰撞點資訊給到各MeshDeformer去做Vertex更新 */
+            foreach (MeshDeformer meshDeformer in meshDeformers)
+            {
+                meshDeformer.isCollided = true;
+                meshDeformer.Deforming(other);
+            }
+            cooldownTime = deformInterval;
         }
 	}
 
