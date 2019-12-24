53f4fc829e28dba83bc4d18bcf4b4ee364002a90
MaxHsu88 <maxsyu88@gmail.com>
2019-12-24 22:06:46 +0800

    Adding Pause Menu

---
+++ Assets/Scripts/UI Control/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    //public GameObject gameSceneUI;
+    public SceneFader sceneFader;
+    public string menuSceneName = "MainMenu";
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        pauseMenuUI.SetActive(!pauseMenuUI.activeSelf);
+        //gameSceneUI.SetActive(!gameSceneUI.activeSelf);
+
+        if (pauseMenuUI.activeSelf)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Retry()
+    {
+        Toggle();
+        sceneFader.FadeTo(SceneManager.GetActiveScene().name);
+    }
+
+    public void Menu()
+    {
+        Toggle();
+        sceneFader.FadeTo(menuSceneName);
+    }
+}
---
+++ Assets/Scripts/UI Control/SceneFader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image img;
+    public AnimationCurve curve;
+
+    void Start ()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo (string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+
+    IEnumerator FadeIn ()
+    {
+        float t = 1f;
+
+        while (t > 0f)
+        {
+            t -= Time.unscaledDeltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color (0f, 0f, 0f, t);
+            yield return 0;
+        }
+    }
+
+    IEnumerator FadeOut (string scene)
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.unscaledDeltaTime;
+            float a = curve.Evaluate(t);
+            img.color = new Color (0f, 0f, 0f, t);
+            yield return 0;
+        }
+
+        SceneManager.LoadScene(scene);
+    }
+}
