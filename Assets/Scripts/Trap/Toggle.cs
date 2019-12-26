8c8fd0aec0a0caf23b0d8e21cc0129a427a0e744
MaxHsu88 <maxsyu88@gmail.com>
2019-12-26 14:56:47 +0800

    Trap Particle System, Sprite & MusicController

--- Assets/Scripts/Trap/Toggle.cs
+++ Assets/Scripts/Trap/Toggle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Toggle : MonoBehaviour
 {
@@ -10,16 +11,33 @@ public class Toggle : MonoBehaviour
     public bool canAttract = true;    // 管控Attractor功能
     public bool canEject = true;      // 管控Ball Eject功能
     public float shutdownProb = 0.6f; // 機關關閉的機率
+    public Sprite NorthSprite;        // N極時的Sprite 
+    public Sprite SouthSprite;        // S極時的Sprite
+
+    //public Canvas SpriteCanvas;       // 顯示圖片的Canvas
+    public Image SpriteImage;         // 顯示圖片的Image
+    public ParticleSystem NorthParticle;  // N極時的粒子效果
+    public ParticleSystem SouthParticle;  // S極時的粒子效果
     float randomWaitTime;
     MeshRenderer meshRenderer;
     Attractor attractor;
     BallEject ballEject;
+
     void Start()
     {
+        NorthParticle = Instantiate(NorthParticle, transform.position, Quaternion.Euler(-90f, 0, 0));
+        SouthParticle = Instantiate(SouthParticle, transform.position, Quaternion.Euler(-90f, 0, 0));
+        NorthParticle.transform.parent = this.transform;
+        SouthParticle.transform.parent = this.transform;
+        StopParticleSystem();
+
         meshRenderer = GetComponent<MeshRenderer> ();
         attractor = GetComponent<Attractor> ();
         ballEject = GetComponent<BallEject> ();
+
         meshRenderer.material = OffMaterial;
+        SpriteImage.enabled = false;
+
         StartCoroutine("ToggleTrigger");          // 讓關卡能夠隨機時間打開及關閉
     }
 
@@ -33,17 +51,24 @@ public class Toggle : MonoBehaviour
                 meshRenderer.material = OffMaterial;
                 attractor.enabled = false;
                 ballEject.enabled = false;
+                SpriteImage.enabled = false;
+                StopParticleSystem();
                 randomWaitTime = Random.Range(1.0f, 4.0f);
             }
             else
             {
+                SpriteImage.enabled = true;
                 randNumber = Random.Range(0f, 1f);
                 /* 一半機率為N極, 一半機率為S極 */
                 if (randNumber >= 0.5f) {
                     meshRenderer.material = OnNorthMaterial;   // N極
+                    SpriteImage.sprite = NorthSprite;
+                    NorthParticle.Play();
                     attractor.magnetPole = 1;      
                 } else {
                     meshRenderer.material = OnSouthMaterial;   // S極
+                    SpriteImage.sprite = SouthSprite;
+                    SouthParticle.Play();
                     attractor.magnetPole = -1;
                 }
 
@@ -57,6 +82,12 @@ public class Toggle : MonoBehaviour
 
             yield return new WaitForSeconds(randomWaitTime);   // 等待一個隨機時間
         }
+    }
+
+    void StopParticleSystem()
+    {
+        NorthParticle.Stop();
+        SouthParticle.Stop();
     }
 
 
